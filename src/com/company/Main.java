f00fd5210dbf5fb032bc2d032f2acfdf75d8a18b
jonathan <jxf131930@utdallas.edu>
2016-01-24 20:25:56 -0600

    using multi-dimensional array to represent the 4x4 matrix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,48 +7,70 @@ public class Main {
         String temp = "";
         int x = 0;
 
-	    String[] a1 = {"A","B","C","D"};
-        String[] a2 = {"E","F","G","H"};
-        String[] a3 = {"I","J","K","L"};
-        String[] a4 = {"M","N","O","P"};
+        String[][] a = new String[4][4];
+        String[][] b = new String[4][4];
 
-        print(a1);
-        print(a2);
-        print(a3);
-        print(a4);
+        a[0][0] = "A";
+        a[0][1] = "B";
+        a[0][2] = "C";
+        a[0][3] = "D";
+        a[1][0] = "E";
+        a[1][1] = "F";
+        a[1][2] = "G";
+        a[1][3] = "H";
+        a[2][0] = "I";
+        a[2][1] = "J";
+        a[2][2] = "K";
+        a[2][3] = "L";
+        a[3][0] = "M";
+        a[3][1] = "N";
+        a[3][2] = "O";
+        a[3][3] = "P";
 
-        String[] b1 = {".",".",".","."};
-        String[] b2 = {".",".",".","."};
-        String[] b3 = {".",".",".","."};
-        String[] b4 = {".",".",".","."};
+        print(a);
 
-        for(int a = 0; a < a1.length; a++){
-            for(int i = 0; i < b1.length ; i++){
-                if(a == 0 ){ x = 3; temp = a1[i];}
-                if(a == 1 ){ x = 2; temp = a2[i];}
-                if(a == 2 ){ x = 1; temp = a3[i];}
-                if(a == 3 ){ x = 0; temp = a4[i];}
+        b[0][0] = "";
+        b[0][1] = "";
+        b[0][2] = "";
+        b[0][3] = "";
+        b[1][0] = "";
+        b[1][1] = "";
+        b[1][2] = "";
+        b[1][3] = "";
+        b[2][0] = "";
+        b[2][1] = "";
+        b[2][2] = "";
+        b[2][3] = "";
+        b[3][0] = "";
+        b[3][1] = "";
+        b[3][2] = "";
+        b[3][3] = "";
 
-                if(i == 0){ b1[x] = temp;}
-                if(i == 1){ b2[x] = temp;}
-                if(i == 2){ b3[x] = temp;}
-                if(i == 3){ b4[x] = temp;}
+
+        for(int c = 0; c < 4; c++){
+            for(int i = 0; i < 4 ; i++){
+                if(c == 0 ){ x = 3; }
+                if(c == 1 ){ x = 2; }
+                if(c == 2 ){ x = 1; }
+                if(c == 3 ){ x = 0; }
+
+                temp = a[c][i];
+                b[i][x] = temp;
             }
         }
 
         System.out.print("\n" + "inverted" + "\n" + "\n");
 
-        print(b1);
-        print(b2);
-        print(b3);
-        print(b4);
+        print(b);
 
     }
 
-    public static void print(String[] t){
-        for(int x = 0; x < 4; x++ ){
-            System.out.print(t[x] + ",");
+    public static void print(String[][] t){
+        for(int x = 0; x < 4; x++ ) {
+            for (int y = 0; y < 4; y++) {
+                System.out.print(t[x][y] + ",");
+                }
+            System.out.print("\n");
         }
-        System.out.print("\n");
     }
 }
